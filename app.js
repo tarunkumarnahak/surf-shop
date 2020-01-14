7679d1181406631621590f7267265d1d8800fc8a
tarunkumarnahak <tarunkumarnahak89@gmail.com>
2020-01-15 00:26:09 +0530

    Add Index

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+//const usersRouter = require('./routes/users');
 const posts = require('./routes/posts');
 const reviews = require('./routes/reviews');
 
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 app.use('/posts', posts);
 app.use('/posts/:id/reviews', reviews);
 
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,51 @@ router.get('/', (req, res, next)=> {
   res.render('index', { title: 'Surf Shop -Home' });
 });
 
+/* GET register. */
+router.get('/register', (req, res, next)=> {
+  res.send('GET /register');
+});
+/* Post register. */
+router.post('/register', (req, res, next)=> {
+  res.send('/POST /register');
+});
+
+/* GET login. */
+router.get('/login', (req, res, next)=> {
+  res.send('GET /login');
+});
+
+/* Post login. */
+router.post('/login', (req, res, next)=> {
+  res.send('POST /login');
+});
+
+/* get /profile . */
+router.get('/profile', (req, res, next)=> {
+  res.send('POST /profile');
+});
+
+/* PUT /profile/:user_id . */
+router.put('/profile/:user_id', (req, res, next)=> {
+  res.send('GET /profile/:user_id');
+});
+
+/* get /forgot. */
+router.get('/forgot', (req, res, next)=> {
+  res.send('GET /forgot');
+});
+
+/* put /forgot. */
+router.put('/forgot', (req, res, next)=> {
+  res.send('PUT /forgot');
+});
+/* GET /reset. */
+router.get('/reset/:token', (req, res, next)=> {
+  res.send('GET /reset/:token');
+});
+/* put /reset. */
+router.put('/reset/:token', (req, res, next)=> {
+  res.send('PUT /reset/:token');
+});
+
 module.exports = router;
